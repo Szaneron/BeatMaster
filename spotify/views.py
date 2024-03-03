91e0e2acec8d1bd4444e6662eba6f05d40fc8218
Szaneron <szaneron@gmail.com>
2024-03-03 12:32:42 +0100

    functionality to play and pause music with Spotify Api created

--- spotify/views.py
+++ spotify/views.py
@@ -5,7 +5,8 @@ from rest_framework import status
 from rest_framework.response import Response
 
 from backend.models import Room
-from .util import update_or_create_user_tokens, is_spotify_authenticated, execute_spotify_api_request
+from .util import update_or_create_user_tokens, is_spotify_authenticated, execute_spotify_api_request, play_song, \
+    pause_song
 import os
 
 from dotenv import load_dotenv
@@ -103,3 +104,25 @@ class CurrentSong(APIView):
         }
 
         return Response(song, status=status.HTTP_200_OK)
+
+
+class PauseSong(APIView):
+    def put(self, request):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)[0]
+        if self.request.session.session_key == room.host or room.guest_can_pause:
+            pause_song(room.host)
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        return Response({}, status=status.HTTP_403_FORBIDDEN)
+
+
+class PlaySong(APIView):
+    def put(self, request):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)[0]
+        if self.request.session.session_key == room.host or room.guest_can_pause:
+            play_song(room.host)
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        return Response({}, status=status.HTTP_403_FORBIDDEN)
