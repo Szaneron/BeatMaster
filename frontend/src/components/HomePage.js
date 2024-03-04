4fb591d9ebd081e9e3f98dfa30be0d75a9921312
Szaneron <szaneron@gmail.com>
2024-03-04 18:34:32 +0100

    card style changes

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -28,7 +28,7 @@ export default class HomePage extends Component {
             <Grid container spacing={3}>
                 <Grid item xs={12} align="center">
                     <Typography variant="h3" compact="h3">
-                        House Party
+                        Beat Master
                     </Typography>
                 </Grid>
                 <Grid item xs={12} align="center">
